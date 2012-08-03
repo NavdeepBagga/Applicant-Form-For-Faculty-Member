bf757645f489c49af8bfb59953f6be798f824220
NavdeepBagga <gottarocknow@gmail.com>
2012-08-03 10:49:51 +0430

    database file

---
+++ Db.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+* This file contains database information  
+* Need of customized Survey prefix and title 
+*        
+* @author      Navdeep Bagga
+* @authorEmail admin@navdeepbagga.com
+* @category    database information
+* @copyright   Copyright (c) June-July Testing and Consultancy Cell (http://www.navdeepbagga.com)
+* @license     General Public License
+* @version     $Id:Db.php 2012-01-08 $
+*/
+
+// Database information
+$database    = 'limesurvey';
+$dbHost      = 'localhost';
+$dbUser      = 'root';
+$dbPass      = 'lug';
+$connection  = mysql_connect ($dbHost, $dbUser, $dbPass);
+mysql_select_db ($database, $connection);
+
+//Survey prefix and title	
+$surveyTitle = 'Application Form for Faculty Position';
+$prefixTable = 'lime_';
+//Tables
+
+$quesTable   = $prefixTable . 'questions';
+$langTable   = $prefixTable . 'surveys_languagesettings';
+$ansTable    = $prefixTable . 'answers';
+
+?>
