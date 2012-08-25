9f8f1dbeb49cfa450177593895fad20684ade023
NavdeepBagga <gottarocknow@gmail.com>
2012-08-25 14:35:01 +0430

    QuesProcess.php

--- QuesProcess.php
+++ QuesProcess.php
@@ -28,119 +28,183 @@ require_once 'UserDetail.php';
 */
 class Ques_Processor
 {	
-    /**
-     * This function retrieves specific column value from question table
-     * 
-     * @arguments column_name,question_table,where_column,selected_value
-     * 
-     * @return    columnvalue_of_question
-     */
-    function createColumn($select, $table, $column, $title)
+	/**
+	 * This function retrieves specific column value from question table
+	 * 
+	 * @arguments column_name,question_table,where_column,selected_value
+	 * 
+	 * @return    columnvalue_of_question
+     */		
+	function createColumn($select, $table, $column, $title)
     {
-        $selectCol= mysql_fetch_array(mysql_query("SELECT `$select` From `$table` " 
-                  . "WHERE `$column`='$title' "
-                  . "ORDER BY `$select` DESC LIMIT 1;"));
+        if(is_array($select))
+        {
+            foreach($select as $select1)
+            {
+                $selectArray = mysql_fetch_array(mysql_query("SELECT $select1 from `$table` " 
+                             . "WHERE `$column` = '$title' "));
+                $fetchArray [] = $selectArray [$select1];
+            }
+            return $fetchArray;
+        }
+        $selectCol = mysql_fetch_array(mysql_query("SELECT `$select` from `$table` " 
+                   . "WHERE `$column` = '$title' "
+                   . "ORDER BY `$select` DESC LIMIT 1;"));
         $fetchCol = $selectCol [$select];	
         return $fetchCol;
     }	
-
-    /**
-     * This function retrieves all information like question id, type and group id  of given question
-     * 
-     * @arguments last_question_id, question_table
-     * 
-     * @return    array of gid,qid,type of question
+	
+	/**
+	 * This function retrieves all information like question id, type and group id  of given question
+	 * 
+	 * @arguments last_question_id, question_table
+	 * 
+	 * @return    array of gid,qid,type of question
      */		
-    function getQuestionInfo($lastQid, $table)
+	function getQuestionInfo($lastQid, $table)
     {
-        for ($ques = 1; $ques <= $lastQid; $ques ++)
+        for ($ques = 1; $ques <= $lastQid; $ques++)
         {
             $questionQid [] = $ques;
         }
         $counterA = 0;
         foreach($questionQid as $singleQid) 
-            {
-                $selectQuesInfo = mysql_query("SELECT `qid`, `gid`, `type` From `$table` "
-                                . "WHERE `qid`='$singleQid'");
-                $fetchQuesInfo  = mysql_fetch_array($selectQuesInfo);
-                $infoQues [$counterA] [qid]  = $fetchQuesInfo ['qid'];	
-                $infoQues [$counterA] [gid]  = $fetchQuesInfo ['gid'];		
-                $infoQues [$counterA] [type] = $fetchQuesInfo ['type'];		
-                $counterA++;
-            }
+        {
+            $selectQuesInfo = mysql_query("SELECT `qid`, `gid`, `type`, `other` from `$table` "
+                            . "WHERE `qid` = '$singleQid'");
+            $fetchQuesInfo  = mysql_fetch_array($selectQuesInfo);
+            $infoQues [$counterA] [qid]  = $fetchQuesInfo ['qid'];	
+            $infoQues [$counterA] [gid]  = $fetchQuesInfo ['gid'];		
+            $infoQues [$counterA] [type] = $fetchQuesInfo ['type'];	
+            $infoQues [$counterA] [other] = $fetchQuesInfo ['other'];		
+        $counterA++;
+        }
         return $infoQues;	
     }
 	
-    /**
-     * This function check the type of question, according to that make column names
-     * 
-     * @arguments survey id, question information, question table
-     * 
-     * @return    array_of_columnnames_of_question
+	/**
+	 * This function check the type of question, according to that make column names
+	 * 
+	 * @arguments survey id, question information, question table
+	 * 
+	 * @return    array_of_columnnames_of_question
      */	
-    function questionColumnCreator ($constSid, $quesInfo, $table)
+	function questionColumnCreator ($constSid, $quesInfo, $table)
     {			
         foreach ($quesInfo as $singleInfo)
-        {
+        {	       
             if (($singleInfo ['type'] == ";")
                 || ($singleInfo ['type'] == ":")
             ) {	
-                $selectUnderscoreColm = mysql_query("SELECT DISTINCT `title`, `gid` , `parent_qid` from `$table` "
-                                      . "WHERE `parent_qid`='$singleInfo[qid]' and `gid`='$singleInfo[gid]'");
+                $selectUnderscoreColm = mysql_query("SELECT `title`, 'scale_id' from `$table`"
+                                      . "WHERE `parent_qid` = '$singleInfo[qid]' and `scale_id` = '0' ");
                 while($fetchUnderscoreColm = mysql_fetch_array($selectUnderscoreColm))	
-                {							
-                    $dinstinctInfo ['title'] = $fetchUnderscoreColm ['title'];
-                    $dinstinctInfo ['gid']   = $fetchUnderscoreColm ['gid'];
-                    $dinstinctInfo ['parent_qid'] = $fetchUnderscoreColm['parent_qid'];
-                            $selectRepeatColm = mysql_query("SELECT `title` from `$table` "
-                                              . "WHERE `parent_qid`='$singleInfo[qid]' and `gid`='$singleInfo[gid]' "
-                                              . "GROUP BY `title` HAVING COUNT(*) > 1 ");
-                            while($fetchRepeatColm = mysql_fetch_array($selectRepeatColm))
-                            {
-                                $compoundValue[] = $constSid."X".$dinstinctInfo['gid']."X".$dinstinctInfo['parent_qid']
-                                . $dinstinctInfo['title']."_".$fetchRepeatColm['title'];
-                            }	
+                {
+                        $selectRepeatColm = mysql_query("SELECT `title` from `$table` "
+                                          . "WHERE `parent_qid` = '$singleInfo[qid]' and `scale_id` = '1' ");
+                        while($fetchRepeatColm = mysql_fetch_array($selectRepeatColm))
+                        {
+                            $compoundValue [] = $constSid."X".$singleInfo ['gid'] ."X".$singleInfo ['qid']
+                                              . $fetchUnderscoreColm ['title'] ."_".$fetchRepeatColm ['title'];
+                        }
                 }
-            }
+              }
             elseif (($singleInfo ['type'] == "M")
                     || ($singleInfo ['type'] == "Q")
             ) {	
-                $selectSqColm = mysql_query("SELECT `title`, `gid`, `parent_qid` from `$table` "
-                              . "WHERE `parent_qid`='$singleInfo[qid]' and `gid`='$singleInfo[gid]'");
+                $selectSqColm = mysql_query("SELECT `title` from `$table` "
+                              . "WHERE `parent_qid` = '$singleInfo[qid]'");
                 while($fetchSqColm = mysql_fetch_array($selectSqColm))	
                 {
-                    $compoundValue[] = $constSid."X".$fetchSqColm['gid']."X"
-                    . $fetchSqColm['parent_qid'].$fetchSqColm['title'];
-                }			
-            }
-            else {
-                $selectSimpleColm = mysql_query("SELECT `title`, `gid`, `qid` from `$table` "
-                                  . "WHERE `qid`='$singleInfo[qid]' and `gid`='$singleInfo[gid]' ");
-                while($fetchSimpleColm = mysql_fetch_array($selectSimpleColm))	
-                    {
-                        $compoundValue[] = $constSid."X".$fetchSimpleColm['gid']."X".$fetchSimpleColm['qid'];
-                    }
-            }			
+                    $compoundValue [] = $constSid."X".$singleInfo ['gid']."X"
+                                      . $singleInfo ['qid'].$fetchSqColm ['title'];
+                }
+                if ($singleInfo ['other'] == "Y")
+                {
+                    $compoundValue [] = $constSid."X".$singleInfo ['gid']
+                                      . "X".$singleInfo ['qid']."other";
+                }
+              }
+           elseif ($singleInfo ['type'] == "|")
+           {
+                $compoundValue [] = $constSid."X".$singleInfo ['gid']
+                                  . "X".$singleInfo ['qid'];
+                $compoundValue [] = $constSid."X".$singleInfo ['gid'] ."X"
+                                  . $singleInfo ['qid'] ."_"."filecount";
+           }
+           else {
+               $compoundValue [] = $constSid."X".$singleInfo ['gid']
+                                 . "X".$singleInfo ['qid'] ;
+               if ($singleInfo ['other'] == "Y")
+               {
+                   $compoundValue [] = $constSid."X".$singleInfo ['gid']
+                                     . "X".$singleInfo ['qid'] ."other";
+               }
+           }		
         }
     return $compoundValue;
     }  
-    /**
-     * This function retrieve answer of passing question column names.
-     * 
-     * @arguments column_names,answer_table,where_column,selected_value
-     * 
-     * @return    answer_of_columnvalue
+	/**
+	 * This function retrieve answer of passing question column names.
+	 * 
+	 * @arguments column_names,answer_table,where_column,selected_value
+	 * 
+	 * @return    answer_of_columnvalue
      */	
-    function displayAnswer($columnName, $table)
-    {		
+	function displayAnswer($columnName, $table, $column)
+    {	
+        global $quesTable;
+        global $ansTable;
         foreach($columnName as $singleColumn)
         {
             $selectRow = mysql_query("SELECT `$singleColumn` from `$table` "
-                       . "WHERE `68316X2X31` = 'gottarocknow@gmail.com' ");
+                       . "WHERE `$column` = 'gottarocknow@gmail.com' ");
             while($fetchRow = mysql_fetch_array($selectRow))
             {
-                $collectInfo [] = $fetchRow [$singleColumn];
-            } 
+                if (($fetchRow [$singleColumn] == "A1")
+                    || ($fetchRow [$singleColumn] == "A2")
+                ) {				
+                    $aDivColm = explode("X", $singleColumn);
+                    $selectARows = mysql_query("SELECT answer from `$ansTable` "
+                                 . "WHERE `qid` = '$aDivColm[2]' AND `code` = '$fetchRow[$singleColumn]'");		
+                    while($fetchARows = mysql_fetch_array($selectARows))
+                    {
+                        $collectInfo [] = $fetchARows ['answer'];
+                    }
+                  }
+                elseif ($fetchRow [$singleColumn] == "Y")
+                {
+                    $yDivColm = explode("X", $singleColumn);
+                    $selectY=mysql_query("SELECT `type` from $quesTable where `gid`='$yDivColm[1]' AND `qid`='$yDivColm[2]'	");
+                    while ($fetchY=mysql_fetch_array($selectY))
+					{
+						$yAnswer=$fetchY['type'];
+					}
+					if ($yAnswer == "M")
+					{
+                    $count = $singleColumn [8];
+                    $explodeTitle = explode("S", $yDivColm[2]);
+                    $RowTitle = "S". $explodeTitle [1];
+                    $selectYRows = mysql_query("SELECT `question` "
+                                 . "from `$quesTable` "
+                                 . "WHERE `parent_qid` = '$count' "
+                                 . "AND `gid` = '$yDivColm[1]' "
+                                 . "AND `title` = '$RowTitle' ");
+                    while($fetchYRows = mysql_fetch_array($selectYRows))
+                    {
+                        $collectInfo [] = $fetchYRows ['question'];
+                    }
+                    }
+                    else
+                    {
+						$collectInfo[] = $fetchRow[$singleColumn];
+					}
+               
+                }
+				else {
+                    $collectInfo [] = $fetchRow [$singleColumn];
+                }
+            }
         }
     return $collectInfo;
     }
